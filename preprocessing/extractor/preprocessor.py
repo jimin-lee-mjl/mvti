4161c865408a23eede77e86a61339a1bc1f32067
jimin <jiminaleejung@gmail.com>
2021-04-06 22:10:23 +0900

    feat: preprocessor 함수 수정 및 extractor 관련 함수 모듈화

--- preprocessing/extractor/preprocessor.py
+++ preprocessing/extractor/preprocessor.py
@@ -1,6 +1,8 @@
 import re
+from string import punctuation
 from nltk.corpus import stopwords
-from sw6 import words
+# data 폴더에서 대사를 가져와서 words를 정의해줘야 합니다.
+# from ..data import Scar
 
 
 # 소문자로 만들기
@@ -8,7 +10,7 @@ def lower_words(list):
     new_words = []
 
     for word in list:
-        if word:
+        if len(word) > 2:
             new_words.append(word.lower())
 
     return new_words
@@ -34,22 +36,48 @@ def remove_tags(list):
     return new_list
 
 
-# 기타 이상한 거 제거
+def remove_punc(word):
+    for p in punctuation:
+        word = word.replace(p,"")
+    return word
+
+
+# 특수문자 및 구두점 삭제 
 def remove_extra(list):
     new_list = []
 
     for word in list:
-        new_word = word.replace("–","").replace("!","").replace(":","").replace("-","")
-        new_word = new_word.replace(".","").replace("?","")
+        new_word = remove_punc(word)
         new_list.append(new_word)
     
     return new_list
 
 
+# 대명사+조동사 삭제 
+def remove_auxiliary(list):
+    filtered_words = [word for word in list if "’" not in word or "'" not in word]
+    return filtered_words
+
+
+# 축약어의 앞부분(명사)만 가져오기
+def split_aux(list):
+    new_list = []
+
+    for word in list:
+        if "'" in word:
+            set = word.split("'")
+            if len(set[0]) > 2:
+                new_list.append(set[0])
+        else:
+            new_list.append(word)
+
+    return new_list
+
+
 # file로 export하기
 def exporter(filename):
-    tag_removed_list = remove_tags(words)
-    extra_removed_list = remove_extra(tag_removed_list)
+    words_wo_aux_verbs = split_aux(words)
+    extra_removed_list = remove_extra(words_wo_aux_verbs)
     lowered_words = lower_words(extra_removed_list)
     words_wo_stopwords = remove_stopwords(lowered_words)
     
