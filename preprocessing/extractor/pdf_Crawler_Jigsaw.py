f19c674a087b0792e1ac36e9758fcfe8ab398ef8
HyunAm <tlfgjawlq@naver.com>
2021-04-06 17:11:27 +0900

    Feat : 전처리 코드 추가, 데이터 추가
    플래쳐, 직소, 조커 크롤러 추가
    플래쳐, 직소, 조커 대화 데이터 추가

--- preprocessing/extractor/pdf_Crawler_Jigsaw.py
+++ preprocessing/extractor/pdf_Crawler_Jigsaw.py
@@ -1,15 +1,13 @@
-fl_text = []
+import itertools
+
 text = []
-# charater = []
 character = []
-with open('Whiplash.txt') as f:
+with open('saw.txt') as f:
     for line in f:
-        text.append(line.replace('\n', '').replace('.', ''))
+        text.append(line.replace('\n', '').replace('.', '').strip())
 # print(text)
 talk = []
 character = []
-
-
 for i in text:
     if i.isupper():
         name = i
@@ -23,5 +21,13 @@ for i in text:
         talk = []
     else:
         talk.append(i)
-fl_text = [i[1] for i in character if i[0] == "FLETCHER"]
-print(fl_text)
+
+jigsaw_text = [i[1] for i in character if i[0] == "JIGSAW"]
+
+jigsaw_string = ''.join(list(itertools.chain(*jigsaw_text)))
+
+
+with open("../data/jigsaw.txt", "w") as f:
+    for i in jigsaw_string:
+        i.replace('\t', '').replace('\n', '').strip()
+        f.write(i)
