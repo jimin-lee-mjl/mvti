4a68678532568af211739faa8445eb5768b0222b
Sowon Jung <swj960515@gmail.com>
2021-04-07 05:23:25 +0000

    Deleted preprocessing/data/HannibalLecter.py, preprocessing/data/HannibalLecter.txt, preprocessing/data/JimMoriarty.py, preprocessing/data/JimMoriarty.txt, preprocessing/data/Scar.py, preprocessing/data/Scar.txt, preprocessing/data/Thanos.py, preprocessing/data/Thanos.txt, preprocessing/data/Vader.py, preprocessing/data/Vader.txt, preprocessing/data/__init__.py, preprocessing/data/fletcher.txt, preprocessing/data/jigsaw.txt, preprocessing/data/joker.txt, preprocessing/extractor/Whiplash.pdf, preprocessing/extractor/__init__.py, preprocessing/extractor/htmlCrawler.py, preprocessing/extractor/pdf_Crawler_Jigsaw.py, preprocessing/extractor/pdf_Crawler_Joker.py, preprocessing/extractor/pdf_Crawler_fletcher.py, preprocessing/extractor/preprocessor.py, preprocessing/extractor/saw.txt, preprocessing/extractor/swCrawler.py, preprocessing/extractor/test.txt, preprocessing/extractor/thedarkknight-screenplay.pdf, preprocessing/extractor/txtCrawler.py, preprocessing/extractor/villainExtractor.py, preprocessing/test/__init__.py, preprocessing/test/test_preprocess.py, preprocessing/.gitkeep, preprocessing/__init__.py, wordCloudImgs/HannibalLecter.png, wordCloudImgs/JimMoriarty.png, wordCloudImgs/Scar.png, wordCloudImgs/Thanos.png, wordCloudImgs/Vader.png, wordCloudImgs/wc.py files

--- preprocessing/test/test_preprocess.py
+++
@@ -1,9 +0,0 @@
-from string import punctuation
-from ..extractor import preprocessor
-
-
-def test_remove_punctuation():
-    list = ['hi"', ':', ',ss', 'dkso']
-    pre_list = preprocessor.remove_extra(list)
-
-    assert pre_list == ['hi', '', 'ss', 'dkso']
