0a57e1490cb60e0ef38064748a847e0371421611
HyunAm <tlfgjawlq@naver.com>
2021-04-07 17:19:49 +0900

    Feat : django character app 추가 character를 작업하기 위한 app을 등록해 주었습니다.

--- backend/movie_project/settings.py
+++ backend/movie_project/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'character',
 ]
 
 ROOT_URLCONF = 'movie_project.urls'
