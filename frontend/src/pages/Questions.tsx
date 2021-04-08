e67d04951779b97cbd6320530f20b685270982fb
Sowon Jung <swj960515@gmail.com>
2021-04-08 19:36:41 +0000

    Feat: Question Page 구성 Components 구현
    - prettier 설정 변경
    - npm 명령어 scripts 수정

---
+++ frontend/src/pages/Questions.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import QuestionContainer from '../containers/QuestionContainer';
+
+const Questions = () => {
+  return (
+    <div>
+      <QuestionContainer />
+    </div>
+  );
+};
+
+export default Questions;
