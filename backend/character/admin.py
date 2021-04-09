cdbb0beba35291c26f7c2176f06bfab04cffeb68
HyunAm <tlfgjawlq@naver.com>
2021-04-09 17:28:25 +0900

    Feat : model추가, Admin페이지 구현
    Model추가, Admin 페이지 구현 만약에 수정사항 있으면 바로 수정해야합니다.

--- backend/character/admin.py
+++ backend/character/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Question, Sentiment, SentimentCharacter, SentimentChoice, Character, Choice
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Sentiment)
+admin.site.register(SentimentCharacter)
+admin.site.register(SentimentChoice)
+admin.site.register(Character)
+admin.site.register(Choice)
