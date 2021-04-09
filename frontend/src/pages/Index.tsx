b15236328150353f6fbbbafe026c53b6872bebcf
Seongmin <seongminha0910@gmail.com>
2021-04-09 07:10:35 +0000

    Fix: Preview 이미지 추가

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -11,7 +11,7 @@ const Index = () => {
       </Box>
       <p>10가지 문항으로 나의 악당 성향 파악하기</p>
       <Box component="span" m={1}>
-        이미지 넣기
+        <img src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fe1f0dd89-223b-4236-8e71-3f50d378b46e%2Fdarkknight.jpg?table=block&id=6e52f522-7077-45a1-b4fe-34be07b748c6&width=960&userId=a4e51eef-8052-4c1a-aa01-db7663147cd6&cache=v2" />
       </Box>
       <br />
       <Button variant="contained" color="primary" href="#contained-buttons">
