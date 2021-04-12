be5a1e5541a73fd145e00b84ea6b36f00fa1b203
HyunAm <tlfgjawlq@naver.com>
2021-04-12 22:38:49 +0900

    Feat : 데이터 분석 이미지 추가 및 api 샘플 구현
    분석한 이미지를 출력하기 위한 api 샘플 구현

--- backend/character/models.py
+++ backend/character/models.py
@@ -6,28 +6,28 @@ from django.db import models
 class Character(models.Model):
     name = models.CharField('character name', max_length=100)
     wc_url = models.URLField('wordcloud url')
-    character_img_url = models.URLField('character img url', default='')
+    character_img_url = models.URLField('character img url', default='', null=True)
 
 
 class Sentiment(models.Model):
-    name = models.CharField('sentiment name', max_length=100)
+    name = models.CharField('sentiment name', max_length=100, null=True)
 
 
 class SentimentCharacter(models.Model):
-    character = models.ForeignKey(Character, on_delete=models.CASCADE, blank=True)
-    sentiment = models.ForeignKey(Sentiment, on_delete=models.CASCADE, blank=True)
-    rate = models.FloatField('rate')
+    character = models.ForeignKey(Character, on_delete=models.CASCADE, blank=True, null=True)
+    sentiment = models.ForeignKey(Sentiment, on_delete=models.CASCADE, blank=True, null=True)
+    rate = models.FloatField('rate', null=True)
 
 
 class Question(models.Model):
-    content = models.TextField('question_content')
+    content = models.TextField('question_content', null=True)
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, blank=True)
-    content = models.TextField('choice_content')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, blank=True, null=True)
+    content = models.TextField('choice_content', null=True)
 
 
 class SentimentChoice(models.Model):
-    sentiment = models.ForeignKey(Sentiment, on_delete=models.CASCADE, blank=True)
-    choice = models.ForeignKey(Choice, on_delete=models.CASCADE, blank=True)
+    sentiment = models.ForeignKey(Sentiment, on_delete=models.CASCADE, blank=True, null=True)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE, blank=True, null=True)
