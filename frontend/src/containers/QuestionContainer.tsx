36149b3c7b89e1222ffe66c44c7248f69ee71d61
SeongminHa <seongminha0910@gmail.com>
2021-04-15 20:04:23 +0000

    Feat: result array 구현

--- frontend/src/containers/QuestionContainer.tsx
+++ frontend/src/containers/QuestionContainer.tsx
@@ -1,30 +1,32 @@
-import React, { useState, useRef } from 'react';
-import QuestionHeader from '../components/sentiment_test/QuestionHeader';
-import ProgressBar from '../components/sentiment_test/ProgressBar';
-import type { question } from '../data/QuestionList';
-import questionList from '../data/QuestionList';
+import React, { useState, useRef } from "react";
+import QuestionHeader from "../components/sentiment_test/QuestionHeader";
+import ProgressBar from "../components/sentiment_test/ProgressBar";
+import type { question } from "../data/QuestionList";
+import questionList from "../data/QuestionList";
 
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 
 const QuestionContainer = () => {
-  const [question, setQuestion] = useState(questionList['questionList'][0]);
-  const [results, setResults] = useState<Array<number>>([]);
+  const [question, setQuestion] = useState(questionList["questionList"][0]);
+  const [results, setResults] = useState<Array<string>>([]);
   const currentId = useRef<number>(0);
-  const changeQuestion = (type: number) => {
+  const changeQuestion = (type: string) => {
     currentId.current += 1;
-    setQuestion(questionList['questionList'][currentId.current]);
+    setQuestion(questionList["questionList"][currentId.current]);
     addResults(type);
   };
-  const addResults = (type: number) => {
+  const addResults = (type: string) => {
     setResults([...results, type]);
   };
-  const sentenceItems = questionList['questionList'][currentId.current]['options'].map((sentence: string) => (
-    <>
-      <Button variant='contained' color='primary' onClick={() => changeQuestion(1)}>
-        {sentence}
-      </Button>
-    </>
-  ));
+  const sentenceItems = questionList["questionList"][currentId.current]["options"].map(
+    (options: Array<string>) => (
+      <>
+        <Button variant='contained' color='primary' onClick={() => changeQuestion(options[0])}>
+          {options[1]}
+        </Button>
+      </>
+    ),
+  );
   return (
     <div>
       <ProgressBar stepIndex={currentId.current} />
@@ -38,8 +40,8 @@ QuestionContainer.defaultProps = {
   questions: [
     {
       number: 1,
-      sentence: 'This is Example Question',
-      options: ['dsfdf', 'ddffff'],
+      sentence: "This is Example Question",
+      options: ["dsfdf", "ddffff"],
     },
   ],
 };
