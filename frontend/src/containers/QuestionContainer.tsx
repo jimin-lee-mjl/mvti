b1ee6e42a51486b73ceaa1b1c790a2426ffb571d
SeongminHa <seongminha0910@gmail.com>
2021-04-15 07:12:46 +0000

    Feat: 문항 데이터 호출, 선택지 버튼 생성

--- frontend/src/containers/QuestionContainer.tsx
+++ frontend/src/containers/QuestionContainer.tsx
@@ -1,35 +1,34 @@
 import React, { useState, useRef } from 'react';
 import QuestionHeader from '../components/sentiment_test/QuestionHeader';
+import ProgressBar from '../components/sentiment_test/ProgressBar';
+import type { question } from '../data/QuestionList';
+import questionList from '../data/QuestionList';
+
 import Button from '@material-ui/core/Button';
 
-type question = {
-  number: number;
-  sentence: string;
-  firstOption: string;
-  secondOption: string;
-};
-
-type QuestionContainerProps = {
-  questions: Array<question>;
-};
-
-const QuestionContainer = ({ questions }: QuestionContainerProps) => {
-  const [question, setQuestion] = useState<question>(questions[0]);
+const QuestionContainer = () => {
+  const [question, setQuestion] = useState(questionList['questionList'][0]);
   const [results, setResults] = useState<Array<number>>([]);
   const currentId = useRef<number>(0);
   const changeQuestion = (type: number) => {
     currentId.current += 1;
-    setQuestion(questions[currentId.current]);
+    setQuestion(questionList['questionList'][currentId.current]);
     addResults(type);
   };
   const addResults = (type: number) => {
     setResults([...results, type]);
   };
+  const sentenceItems = questionList['questionList'][currentId.current]['options'].map((sentence: string) => (
+    <>
+      <Button variant='contained' color='primary' onClick={() => changeQuestion(1)}>
+        {sentence}
+      </Button>
+    </>
+  ));
   return (
     <div>
       <QuestionHeader question={question} />
-      <Button onClick={() => changeQuestion(1)}>{question.firstOption}</Button>
-      <Button onClick={() => changeQuestion(2)}>{question.secondOption}</Button>
+      {sentenceItems}
     </div>
   );
 };
@@ -39,8 +38,7 @@ QuestionContainer.defaultProps = {
     {
       number: 1,
       sentence: 'This is Example Question',
-      firstOption: 'yes',
-      secondOption: 'no',
+      options: ['dsfdf', 'ddffff'],
     },
   ],
 };
