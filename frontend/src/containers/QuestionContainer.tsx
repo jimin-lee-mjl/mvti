55acfa24505c7f9dbe7e9259538a55d146c95875
Sowon Jung <swj960515@gmail.com>
2021-04-16 03:43:26 +0000

    Chore: feat/frontend에 대한 충돌 해결

--- frontend/src/containers/QuestionContainer.tsx
+++ frontend/src/containers/QuestionContainer.tsx
@@ -1,24 +1,24 @@
-import React, { useState, useRef } from 'react';
-import QuestionHeader from '../components/sentiment_test/QuestionHeader';
-import ProgressBar from '../components/sentiment_test/ProgressBar';
-import type { question } from '../data/QuestionList';
-import questionList from '../data/QuestionList';
+import React, { useState, useRef } from "react";
+import QuestionHeader from "../components/sentiment_test/QuestionHeader";
+import ProgressBar from "../components/sentiment_test/ProgressBar";
+import type { question } from "../data/QuestionList";
+import questionList from "../data/QuestionList";
 
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 
 const QuestionContainer = () => {
-  const [question, setQuestion] = useState(questionList['questionList'][0]);
+  const [question, setQuestion] = useState(questionList["questionList"][0]);
   const [results, setResults] = useState<Array<number>>([]);
   const currentId = useRef<number>(0);
   const changeQuestion = (type: number) => {
     currentId.current += 1;
-    setQuestion(questionList['questionList'][currentId.current]);
+    setQuestion(questionList["questionList"][currentId.current]);
     addResults(type);
   };
   const addResults = (type: number) => {
     setResults([...results, type]);
   };
-  const sentenceItems = questionList['questionList'][currentId.current]['options'].map((sentence: string) => (
+  const sentenceItems = questionList["questionList"][currentId.current]["options"].map((sentence: string) => (
     <>
       <Button variant='contained' color='primary' onClick={() => changeQuestion(1)}>
         {sentence}
@@ -38,8 +38,8 @@ QuestionContainer.defaultProps = {
   questions: [
     {
       number: 1,
-      sentence: 'This is Example Question',
-      options: ['dsfdf', 'ddffff'],
+      sentence: "This is Example Question",
+      options: ["dsfdf", "ddffff"],
     },
   ],
 };
